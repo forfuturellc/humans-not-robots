695d600bb2712dc0863754d027804605d0eb7b5e
oginga <stevilley@gmail.com>
2015-11-19 14:45:03 +0300

    created home view

--- hnr/views.py
+++ hnr/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from .models import url
 
-# Create your views here.
+from .forms import uri
+
+def home(request):
+	#print 'REQUEST IS:',request
+
+	form=uri()
+	context={'form':form}
+	template="home.html"
+	return render(request,template,context)
+
+def contributors(request):
+	form=uri(request.POST or None)
+
+	if form.is_valid():
+		#print 'IS HERE:',form.cleaned_data
+		print form.cleaned_data['url']
+		data=url(form.cleaned_data['url'])
+
+
+	context={'data':data}
+	template="inhuman.html"
+	return render(request,template,context)
+
