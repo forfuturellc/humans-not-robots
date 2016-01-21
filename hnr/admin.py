85ae2de60ee25f992cd684262c46f4f0cc0f50e3
oginga <stevilley@gmail.com>
2016-01-21 18:29:52 +0300

    registered models to admin

--- hnr/admin.py
+++ hnr/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Humans,SubmitedUrls
 
 # Register your models here.
+class URLAdmin(admin.ModelAdmin):
+		model=SubmitedUrls
+
+class HumansAdmin(admin.ModelAdmin):
+	list_display=['__unicode__','twitter','role']
+	#inlines=[URLAdmin,]
+		
+admin.site.register(Humans,HumansAdmin)
+admin.site.register(SubmitedUrls,URLAdmin)
