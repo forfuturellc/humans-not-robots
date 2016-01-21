d6e77bab772e4d71fc39024d78a05e9bdfba1ebd
oginga <stevilley@gmail.com>
2016-01-21 17:29:24 +0300

    added model classes

--- hnr/models.py
+++ hnr/models.py
@@ -1,49 +1,28 @@
 from django.db import models
-import re
-import urlparse
 import argparse
-import urllib
 #import humanstxt
 
 #regexlib.com/Search.aspx?k=url&AspxAutoDetectCookieSupport=1
+#if website not available heroku returns empty dict
 
-def url(s):
-	'''Takea a url string,validates using re and recreate it to call humans.herokuap.com >>produces 
-		json string which is returned as data:dict
-	 '''
-	pat=re.compile('^(http|https|ftp)\://[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(:[a-zA-Z0-9]*)?/?([a-zA-Z0-9\-\._\?\,\'/\\\+&amp;%\$#\=~])*$')
-	pat2=re.compile('[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(:[a-zA-Z0-9]*)?/?([a-zA-Z0-9\-\._\?\,\'/\\\+&amp;%\$#\=~])*$')
-
-	human_url=None
-
-	if re.match(pat,s) or re.match(pat2,s):
-		print 'Match successful'
-
-		parsed=urlparse.urlparse(s)
-		if parsed.scheme == 'https' :
-			human_url=parsed.netloc+'?use_ssl=true'
-			
-			print human_url
-		elif parsed.scheme == 'http':
-			human_url=parsed.netloc
-
-		elif parsed.path != '' and parsed.scheme =='':
-			human_url=parsed.path
-			print human_url
-
-
-		f=urllib.URLopener().open('https://humans.herokuapp.com/'+human_url)
-		data=f.read()
+class Humans(models.Model):
+	twitter=models.URLField(unique=True)
+	name=models.CharField(max_length=70)
+	role=models.CharField(max_length=100)
+	site=models.URLField(null=True)
 	
-		return data
+	timetsamp=models.DateTimeField(auto_now_add=True,auto_now=False)
+	updated=models.DateTimeField(auto_now_add=False,auto_now=True)
+	
+	def __unicode__(self):
+		return self.twitter
 
-	else:print 'Error'
+class SubmitedUrls(models.Model):
+	site=models.URLField(unique=True)
+	site_humans=models.ManyToManyField(Humans)
 
+	def __unicode__(self):
+		return self.site
 
-
-if __name__ == '__main__':
-	parser=argparse.ArgumentParser()
-	parser.add_argument("url",help="Enter valid url",type=str)
-	args=parser.parse_args()
-	url(args.url)
+]
 
