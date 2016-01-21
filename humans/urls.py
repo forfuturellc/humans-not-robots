85305c05fa38f89c464ff01b51be5314bfe369ed
oginga <stevilley@gmail.com>
2016-01-21 17:35:22 +0300

    modified url:Humans

--- humans/urls.py
+++ humans/urls.py
@@ -19,6 +19,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'hnr.views.home',name='home'),
-    url(r'^contributors/$', 'hnr.views.contributors',name='contributors'),
+    url(r'^$', 'hnr.views.humans',name='humans'),
 
 ]
